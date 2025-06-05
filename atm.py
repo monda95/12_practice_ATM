3c1d919f07581a5c9833181a28d9d254f82d2d7b
monda95 <jungmonda@naver.com>
2025-06-05 17:54:38 +0900

    ✨ 출금 기능 추가
    출금 금액을 받아, 잔액에서 차감하는 기능, min으로 마이너스가 되지않는 기능 구현

--- atm.py
+++ atm.py
@@ -4,10 +4,17 @@ balance = 1000
 while True: #조건이 트루일 동안 무한으로 반복
     num = input('사용하실 기능의 번호를 선택해주세요(1,입금, 2.출금, 3.영수증보기, 4.종료): ')
 
+    #2번 출금 기능
+    if num == "2":
+        withdraw_amount = int(input('출금하실 금액을 입력해주세요: '))
+        withdraw_amount = min(balance, withdraw_amount)
+        balance -= withdraw_amount
+        print(f'출금하실 금액은 {withdraw_amount}원 이고, 잔액은 {balance}원 입니다.')
+
     if num == "1":
         deposit_amount = int(input('입금하실 금액을 입력해주세요: '))
         balance += deposit_amount
-        print(f'입급하신 금액은 {deposit_amount}원 이고, {balance}원 입니다.')
+        print(f'입급하신 금액은 {deposit_amount}원 이고, 잔액은 {balance}원 입니다.')
 
     if num == "4":
         print('화면을 종료합니다.')
